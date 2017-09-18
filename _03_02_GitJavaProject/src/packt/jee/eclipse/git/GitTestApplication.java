0969175b2b73ea379ec880eb7a598949bb11c1c7
Sea Monk <simonrizkallah@hotmail.com>
2017-09-18 13:28:16 +0300

    commit#1 from bug#1 branch

--- _03_02_GitJavaProject/src/packt/jee/eclipse/git/GitTestApplication.java
+++ _03_02_GitJavaProject/src/packt/jee/eclipse/git/GitTestApplication.java
@@ -5,5 +5,6 @@ public class GitTestApplication {
 	public static void main(String[] args) {
 		System.out.println("Hello Git...");
 		System.out.println("End.");
+		System.out.println("Hello Git, from branch bug#1234 !!");
 	}
 }
