a5940c99c37fbcc8730948c0d468304417ecc64c
Sea Monk <simonrizkallah@hotmail.com>
2017-09-18 12:05:48 +0300

    commit#2

--- _03_02_GitJavaProject/src/_03_02_GitJavaProject/packt/jee/eclipse/git/GitTestApplication.java
+++
@@ -1,8 +0,0 @@
-package _03_02_GitJavaProject.packt.jee.eclipse.git;
-
-
-public class GitTestApplication {
-	public static void main(String[] args) {
-		System.out.println("Hello Git.......");
-	}
-}
---
+++ _03_02_GitJavaProject/src/packt/jee/eclipse/git/GitTestApplication.java
@@ -0,0 +1,8 @@
+package packt.jee.eclipse.git;
+
+
+public class GitTestApplication {
+	public static void main(String[] args) {
+		System.out.println("Hello Git...");
+	}
+}
