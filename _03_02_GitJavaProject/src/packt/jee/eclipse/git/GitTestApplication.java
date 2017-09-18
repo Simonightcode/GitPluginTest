77164406e2d1072ae30743e2257efe53850bd527
Sea Monk <simonrizkallah@hotmail.com>
2017-09-18 12:07:34 +0300

    commit#3

--- _03_02_GitJavaProject/src/packt/jee/eclipse/git/GitTestApplication.java
+++ _03_02_GitJavaProject/src/packt/jee/eclipse/git/GitTestApplication.java
@@ -4,5 +4,6 @@ package packt.jee.eclipse.git;
 public class GitTestApplication {
 	public static void main(String[] args) {
 		System.out.println("Hello Git...");
+		System.out.println("End.");
 	}
 }
